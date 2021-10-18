08a052a400ba35b159d40bd35cd1c70ef8bbf492
Re9iNee <attarzadeh76@gmail.com>
2021-10-18 17:16:34 +0330

    empty react project

--- src/App.js
+++ src/App.js
@@ -1,31 +1,7 @@
 import React from "react";
-import "./App.css";
-import Nav from "./Nav";
-import Shop from "./Shop";
-import About from "./About";
-import ItemDetail from "./ItemDetail";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
-    return (
-        <Router>
-            <div className='App'>
-                <Nav />
-                <Switch>
-                    <Route path='/' exact component={Home} />
-                    <Route path='/about' component={About} />
-                    <Route path='/shop' exact component={Shop} />
-                    <Route path='/shop/:id' component={ItemDetail} />
-                </Switch>
-            </div>
-        </Router>
-    );
+    return <div></div>;
 };
-
-const Home = () => (
-    <div>
-        <h1>Home Page</h1>
-    </div>
-);
 
 export default App;
--- src/components/Navbar.js
+++
@@ -1,7 +0,0 @@
-import React, { useContext } from "react";
-import { MovieContext } from "../contexts/movieContext";
-
-export default function Nav() {
-    const [movies, setMovies] = useContext(MovieContext);
-    return <h1>We have {movies.length} movies </h1>;
-}

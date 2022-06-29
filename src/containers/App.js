a3b3eddbc71f4665b340c6022144a7c5d0d40367
Re9iNee <attarzadeh76@gmail.com>
2022-06-30 01:43:12 +0430

    setting up env for redux

---
+++ src/containers/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { Router, Route, hashHistory, IndexRoute } from "react-router";
+import { Index } from "./Index";
+
+import Home from "../views/Home/Home";
+import About from "../views/About/About";
+
+const App = () => {
+    return (
+        <Router history={hashHistory}>
+            <Route path='/' component={Index}>
+                <IndexRoute component={Home} />
+                <Route path='home' component={Home} />
+                <Route path='about' component={About} />
+            </Route>
+        </Router>
+    );
+};
+
+export default App;

051b20275e054514b216c8ffdf6d9ddb238bf9c0
Re9iNee <attarzadeh76@gmail.com>
2022-09-02 23:17:29 +0430

    ready for render dashboard

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const App = () => {
-    return <div></div>;
-};
+function App() {
+    const [pState, setPState] = useState(false);
+
+    useEffect(() => {
+        console.log("Use effect parent. " + pState);
+
+        return () => {
+            console.log(
+                "Parent cleanup function called. Current State: " + pState
+            );
+        };
+    }, [pState]);
+
+    const pChangeHandler = () => {
+        setPState((prevP) => {
+            return !prevP;
+        });
+    };
+
+    console.log("Parent component rendered. " + pState);
+    return (
+        <div className='App'>
+            <button onClick={pChangeHandler}>Change P State</button>
+        </div>
+    );
+}
 
 export default App;
